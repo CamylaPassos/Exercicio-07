87ca55569db8d523753fd78af4a4db14d80e2c4f
Myla <Myla@DESKTOP-V9A76BG>
2022-05-26 19:20:17 -0300

    Add project files.

---
+++ Exercicio-07/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numero = 0;
+
+Console.WriteLine("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero >= 0)
+{
+    Console.WriteLine("Este número é POSITIVO!");
+}
+else
+{
+    Console.WriteLine("Este número é NEGATIVO!");
+}
